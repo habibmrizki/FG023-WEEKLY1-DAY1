9ce284f6b2f5b2f1763711ace07e61e63a3b6593
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-02-14 16:54:12 +0700

    change task2.js

--- task4.js
+++ task4.js
@@ -1,10 +1,11 @@
 function transformSentence(sentence) {
   const verb = sentence
     .split(" ")
+    .sort((a, b) => b.localeCompare(a))
+    .map((word) => word.toUpperCase())
     .concat("FAZZTRACK")
-    .sort()
-    .join(" ")
-    .toUpperCase();
+    .reverse()
+    .join(" ");
   console.log(verb);
 }
 transformSentence("saya belajar javascript");
