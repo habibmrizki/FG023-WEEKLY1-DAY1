73855170b3043b1f89645629525bc449f8a5c011
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-02-14 17:10:22 +0700

    change task2.js

--- task2.js
+++ task2.js
@@ -34,8 +34,13 @@ const movie = {
   },
   getCast() {
     const cast = this.cast.sort((a, b) => {
-      if (a.name < b.name) return -1;
-      if (a.name < b.name) return 1;
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
     });
     console.log(cast);
   },
